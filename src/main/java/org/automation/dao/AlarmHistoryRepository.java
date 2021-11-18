9c5eecd2b5e1f6823604ad305a322617fd8c48a1
umprasad <umprasad@cisco.com>
2021-11-18 11:32:50 +0530

    changes on query

--- src/main/java/org/automation/dao/AlarmHistoryRepository.java
+++ src/main/java/org/automation/dao/AlarmHistoryRepository.java
@@ -12,6 +12,7 @@ public interface AlarmHistoryRepository extends MongoRepository<AlarmHistory,Str
 	@Query("{enddate:{$lte:?0},type:{$ne:?1}}")
     List<AlarmHistory> findByEndDateLessThanEqualAndTypeNotContain(Date date,String type);
 	
-	@Query("{updatedate:{$gte:?0},enddate:{$lte:?1},type:{$ne:?2}}")
+	//@Query("{updatedate:{$gte:?0},enddate:{$lte:?1},type:{$ne:?2}}")
+	@Query("{enddate:{$gte:?0,$lte:?1},type:{$ne:?2}}")
     List<AlarmHistory> findByEndDateBetweenAndTypeNotContain(Date startDate,Date endDate,String type);
 }
